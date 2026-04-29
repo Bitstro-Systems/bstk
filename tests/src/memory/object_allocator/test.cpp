fb84561cc48e8e89d8d2c697243de71af6e0a186
MateuszJanduraUszu <mjandura03@gmail.com>
2026-04-29 21:17:46 +0200

    Rework `object_allocator` unit test

--- tests/src/memory/object_allocator/test.cpp
+++ tests/src/memory/object_allocator/test.cpp
@@ -3,55 +3,19 @@
 // Copyright (c) Bitstro Systems. All rights reserved.
 // Licensed under BSDL 1.0
 
-#include <bstk/core/impl/utility.hpp>
 #include <bstk/memory/object_allocator.hpp>
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 namespace bs {
-    class _Tracking_allocator : public allocator {
+    class _Fake_allocator : public allocator {
     public:
-        _Tracking_allocator(size_type& _Size, size_type& _Align) noexcept
-            : _Mysize(_Size), _Myalign(_Align) {}
-
-        ~_Tracking_allocator() noexcept override = default;
-
-        _Tracking_allocator()                                      = delete;
-        _Tracking_allocator(const _Tracking_allocator&)            = delete;
-        _Tracking_allocator& operator=(const _Tracking_allocator&) = delete;
-
-        allocator_id id() const noexcept override {
-            return allocator_id{0xFF};
-        }
-
-        size_type max_size() const noexcept override {
-            return 0xFFFF;
-        }
-
-        bool is_equal(const allocator&) const noexcept override {
-            return true;
-        }
-
-        bool is_thread_safe() const noexcept override {
-            return false;
-        }
-
-        [[nodiscard]] pointer allocate(size_type _Size, size_type _Align) override {
-            _Capture_allocation_size(_Size, _Align);
-            return nullptr;
-        }
-
-        void deallocate(pointer, size_type _Size, size_type _Align) override {
-            _Capture_allocation_size(_Size, _Align);
-        }
-
-    private:
-        void _Capture_allocation_size(const size_type _Size, const size_type _Align) noexcept {
-            _Mysize  = bstk::_Align_up_pow_of_2(_Size, _Align);
-            _Myalign = _Align;
-        }
-
-        size_type& _Mysize;
-        size_type& _Myalign;
+        MOCK_METHOD(allocator_id, id, (), (const, noexcept, override));
+        MOCK_METHOD(size_type, max_size, (), (const, noexcept, override));
+        MOCK_METHOD(bool, is_equal, (const allocator&), (const, noexcept, override));
+        MOCK_METHOD(bool, is_thread_safe, (), (const, noexcept, override));
+        MOCK_METHOD(pointer, allocate, (size_type, size_type), (override));
+        MOCK_METHOD(void, deallocate, (pointer, size_type, size_type), (override));
     };
 
     struct alignas(32) _Aligned_type {
@@ -60,71 +24,75 @@ namespace bs {
 
     class object_allocator_test : public ::testing::Test {
     protected:
-        using _Value_type = _Aligned_type;
-        using _Alloc_type = object_allocator<_Value_type>;
-
-        static void SetUpTestSuite() {
-            ::bs::set_allocator(_Real_al);
+        object_allocator_test() noexcept : _Fake(), _Al() {
+            ::bs::set_allocator(_Fake);
         }
 
-        static size_t _Captured_size;
-        static size_t _Captured_align;
-        static _Tracking_allocator _Real_al;
-
-        _Alloc_type _Al;
+        _Fake_allocator _Fake;
+        object_allocator<_Aligned_type> _Al;
     };
 
-    size_t object_allocator_test::_Captured_size  = 0;
-    size_t object_allocator_test::_Captured_align = 0;
-    _Tracking_allocator object_allocator_test::_Real_al(_Captured_size, _Captured_align);
-
     TEST_F(object_allocator_test, id) {
-        EXPECT_EQ(_Al.id(), _Real_al.id());
+        constexpr allocator_id _Id = static_cast<allocator_id>(200);
+        EXPECT_CALL(_Fake, id()).Times(1).WillOnce(::testing::Return(_Id));
+        EXPECT_EQ(_Al.id(), _Id);
     }
 
     TEST_F(object_allocator_test, max_size) {
-        EXPECT_EQ(_Al.max_size(), _Real_al.max_size() / sizeof(_Value_type));
+        constexpr size_t _Raw_max_size = 0xFF80;
+        constexpr size_t _Max_size     = _Raw_max_size / sizeof(_Aligned_type);
+        EXPECT_CALL(_Fake, max_size()).Times(1).WillOnce(::testing::Return(_Raw_max_size));
+        EXPECT_EQ(_Al.max_size(), _Max_size);
     }
 
     TEST_F(object_allocator_test, is_equal) {
-        EXPECT_TRUE(_Al.is_equal(_Real_al));
+        constexpr bool _Equal = true;
+        const _Fake_allocator _Other_fake;
+        EXPECT_CALL(_Fake, is_equal(::testing::Ref(_Other_fake))).Times(1).WillOnce(::testing::Return(_Equal));
+        EXPECT_EQ(_Al.is_equal(_Other_fake), _Equal);
     }
 
     TEST_F(object_allocator_test, is_thread_safe) {
-        EXPECT_EQ(_Al.is_thread_safe(), _Real_al.is_thread_safe());
+        constexpr bool _Thread_safe = true;
+        EXPECT_CALL(_Fake, is_thread_safe()).Times(1).WillOnce(::testing::Return(_Thread_safe));
+        EXPECT_EQ(_Al.is_thread_safe(), _Thread_safe);
     }
 
     TEST_F(object_allocator_test, allocate_type_alignment) {
-        constexpr size_t _Count         = 4;
-        constexpr size_t _Expected_size = _Count * sizeof(_Value_type);
-        (void) _Al.allocate(_Count);
-        EXPECT_EQ(_Captured_size, _Expected_size);
-        EXPECT_EQ(_Captured_align, alignof(_Value_type));
+        _Aligned_type* const _Ptr    = reinterpret_cast<_Aligned_type*>(uintptr_t{0xABCD'9561});
+        constexpr size_t _Count      = 4;
+        constexpr size_t _Align      = 0; // default alignment
+        constexpr size_t _Size       = _Count * sizeof(_Aligned_type);
+        constexpr size_t _Real_align = alignof(_Aligned_type);
+        EXPECT_CALL(_Fake, allocate(_Size, _Real_align)).Times(1).WillOnce(::testing::Return(_Ptr));
+        EXPECT_EQ(_Al.allocate(_Count, _Align), _Ptr);
     }
 
     TEST_F(object_allocator_test, allocate_custom_alignment) {
-        constexpr size_t _Count         = 7;
-        constexpr size_t _Align         = 128;
-        constexpr size_t _Expected_size = bstk::_Align_up_pow_of_2(_Count * sizeof(_Value_type), _Align);
-        (void) _Al.allocate(_Count, _Align);
-        EXPECT_EQ(_Captured_size, _Expected_size);
-        EXPECT_EQ(_Captured_align, _Align);
+        _Aligned_type* const _Ptr = reinterpret_cast<_Aligned_type*>(uintptr_t{0xDA61'789F});
+        constexpr size_t _Count   = 7;
+        constexpr size_t _Align   = 128;
+        constexpr size_t _Size    = _Count * sizeof(_Aligned_type);
+        EXPECT_CALL(_Fake, allocate(_Size, _Align)).Times(1).WillOnce(::testing::Return(_Ptr));
+        EXPECT_EQ(_Al.allocate(_Count, _Align), _Ptr);
     }
 
     TEST_F(object_allocator_test, deallocate_type_alignment) {
-        constexpr size_t _Count         = 15;
-        constexpr size_t _Expected_size = _Count * sizeof(_Value_type);
-        _Al.deallocate(nullptr, _Count);
-        EXPECT_EQ(_Captured_size, _Expected_size);
-        EXPECT_EQ(_Captured_align, alignof(_Value_type));
+        _Aligned_type* const _Ptr    = reinterpret_cast<_Aligned_type*>(uintptr_t{0x5152'AAEB});
+        constexpr size_t _Count      = 15;
+        constexpr size_t _Align      = 0; // default alignment
+        constexpr size_t _Size       = _Count * sizeof(_Aligned_type);
+        constexpr size_t _Real_align = alignof(_Aligned_type);
+        EXPECT_CALL(_Fake, deallocate(_Ptr, _Size, _Real_align)).Times(1);
+        _Al.deallocate(_Ptr, _Count, _Align);
     }
 
     TEST_F(object_allocator_test, deallocate_custom_alignment) {
-        constexpr size_t _Count         = 21;
-        constexpr size_t _Align         = 256;
-        constexpr size_t _Expected_size = bstk::_Align_up_pow_of_2(_Count * sizeof(_Value_type), _Align);
-        _Al.deallocate(nullptr, _Count, _Align);
-        EXPECT_EQ(_Captured_size, _Expected_size);
-        EXPECT_EQ(_Captured_align, _Align);
+        _Aligned_type* const _Ptr = reinterpret_cast<_Aligned_type*>(uintptr_t{0xF9C2'A0E4});
+        constexpr size_t _Count   = 21;
+        constexpr size_t _Align   = 256;
+        constexpr size_t _Size    = _Count * sizeof(_Aligned_type);
+        EXPECT_CALL(_Fake, deallocate(_Ptr, _Size, _Align)).Times(1);
+        _Al.deallocate(_Ptr, _Count, _Align);
     }
 } // namespace bs
